f9389d798f94db87b05020bce57d66828f839fcd
Liam G <liamg10@yahoo.co.uk>
2024-02-16 22:00:59 +0000

    fix: spelling error in polyamory

--- assets/scripts/generator/letter.js
+++ assets/scripts/generator/letter.js
@@ -41,7 +41,7 @@ document.getElementById('loveLetterForm').addEventListener('submit', (event) => 
   ${yourName}`
 
   // Polyamorous letter
-  const polyamourousLetter = `My Dearest ${partnerName},
+  const polyamorousLetter = `My Dearest ${partnerName},
 
   With each passing day, my heart overflows with gratitude for the love and connection we share. The bond we've formed, woven together by trust, ${noun1}, and affection, is a testament to the beauty of polyamory and the depth of our collective love.
 
@@ -79,8 +79,8 @@ document.getElementById('loveLetterForm').addEventListener('submit', (event) => 
     letterOutput.innerText = platonicLetter;
   } else if (letterType === 'Romantic') {
     letterOutput.innerText = romanticLetter;
-  } else if (letterType === 'Polyamourous') {
-    letterOutput.innerText = polyamourousLetter;
+  } else if (letterType === 'Polyamorous') {
+    letterOutput.innerText = polyamorousLetter;
   } else {
     letterOutput.innerText = monogamousLetter;
   }
